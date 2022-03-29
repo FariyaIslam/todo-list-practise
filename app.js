1aafe71c81966778ea525135d3a7aef85341fdc7
Fariya Islam <fariyaislam2096@gmail.com>
2022-03-29 22:04:25 +0600

    day 2  update

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-const input = document.querySelector("input");
-console.inpu
+const full = document.querySelector(".container");
+full.style.backgroundColor = 'orange';
+
+const input = document.querySelector('input');
+const title = document.querySelector('.title');
+
+input.onchange = handleInputChange;
+
+function handleInputChange(e) {
+    console.log(e.target.value);
+    title.textContent = e.target.value;
+}
+
+const colors = ['red', 'green', 'blue'];
+
+// We will impement randomization for our app background color;
+// Todo Add
